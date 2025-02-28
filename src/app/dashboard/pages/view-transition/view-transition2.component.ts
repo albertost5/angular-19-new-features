b4f5a6ba301d5f7de70bbe396d73a3fb3c5b37b7
albertost5 <sancheztorresalberto@gmail.com>
2025-02-28 13:02:50 +0100

    Added app routes

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './view-transition2.component.html',
+  styles: ``
+})
+export default class ViewTransition2Component {
+
+}
