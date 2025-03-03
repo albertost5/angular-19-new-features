52cba27f8b70e9ba8361f93b329428fa9fc09edd
albertost5 <sancheztorresalberto@gmail.com>
2025-03-03 20:11:22 +0100

    Added defer views

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {TitleComponent} from '@shared/title/title.component';
+import {CommonModule} from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [
+    TitleComponent,
+    CommonModule
+  ],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export default class ChangeDetectionComponent {
+  framework = signal({ name: 'Angular', releaseDate: 2016});
+  frameworkProperty = {
+    name: 'React',
+    releaseDate: 2016
+  }
+  currentFramework = computed(() => `Change Detection - ${this.framework().name}`)
 
+  constructor() {
+    setTimeout(() => {
+
+      // OnPush side effect => no update
+      // this.frameworkProperty.name = 'Svelte';
+
+      this.framework.update(current => ({...current, name: 'Astro'}));
+
+      console.log('DONE!');
+    }, 3000)
+  }
 }
--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TitleComponent} from '@shared/title/title.component';
+import {HeavyLoadersSlowComponent} from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
-  imports: [],
+  imports: [
+    TitleComponent,
+    HeavyLoadersSlowComponent,
+  ],
   standalone: true,
   templateUrl: './defer-views.component.html',
   styles: ``
