f455540209d069906b66878248b68cc017d043ee
albertost5 <sancheztorresalberto@gmail.com>
2025-03-03 19:09:25 +0100

    Added title component and control flow logic

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {TitleComponent} from '@shared/title/title.component';
 
 @Component({
-  imports: [],
+  imports: [
+    TitleComponent
+  ],
   standalone: true,
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
+  public showContent = signal<boolean>(false);
+  public frameworks = signal<string[]>(['Angular', 'React', 'Vue', 'Svelte', 'Astro']);
+  public emptyFrameworks = signal<string[]>([]);
+  public randomNumber = signal<number>(-1);
 
+  toggleContent() {
+    this.showContent.update(current => !current);
+  }
+
+  randomFramework() {
+    this.randomNumber.set(Math.floor(Math.random() * 5));
+  }
 }
